6d31a5553e136a987e4816f39e7ba574c081840d
mareadelmar <mar.ortega.v@gmail.com>
2021-12-11 13:13:21 -0300

    Refactor the code to be more declarative

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -14,24 +14,64 @@ const UseState = () => {
 
 	console.log(state);
 
+	const onConfirm = () => {
+		setState({
+			...state,
+			error: false,
+			loading: false,
+			confirmed: true,
+		});
+	};
+
+	const onError = () => {
+		setState({
+			...state,
+			error: true,
+			loading: false,
+		});
+	};
+
+	const onInputChange = newValue => {
+		setState({
+			...state,
+			inputValue: newValue,
+		});
+	};
+
+	const onSubmitCheck = () => {
+		setState({
+			...state,
+			loading: true,
+		});
+	};
+
+	const onDelete = () => {
+		setState({
+			...state,
+			deleted: true,
+		});
+	};
+
+	const onReset = () => {
+		setState({
+			...state,
+			confirmed: false,
+			deleted: false,
+			inputValue: "",
+		});
+	};
+
 	useEffect(() => {
 		console.log("entrando al efecto");
 		if (!!state.loading) {
 			setTimeout(() => {
 				if (state.inputValue !== SECURITY_CODE) {
-					setState({
-						...state,
-						error: true,
-						loading: false,
-					});
+					// al estado de error
+					onError();
 					return;
 				}
-				setState({
-					...state,
-					error: false,
-					loading: false,
-					confirmed: true,
-				});
+				// al estado de confirmación
+				onConfirm();
 			}, 2000);
 		}
 	}, [state.loading]);
@@ -52,67 +92,25 @@ const UseState = () => {
 					placeholder='Código de seguridad'
 					value={state.inputValue}
 					onChange={e => {
-						setState({
-							...state,
-							inputValue: e.target.value,
-						});
+						onInputChange(e.target.value);
 					}}
 				/>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							loading: true,
-						});
-					}}
-				>
-					Comprobar
-				</button>
+				<button onClick={onSubmitCheck}>Comprobar</button>
 			</div>
 		);
 	} else if (state.confirmed && !state.deleted) {
 		return (
 			<>
 				<p>¿estás segurx de eliminar?</p>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							deleted: true,
-						});
-					}}
-				>
-					Sí, eliminar
-				</button>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							confirmed: false,
-							inputValue: "",
-						});
-					}}
-				>
-					No, volver
-				</button>
+				<button onClick={onDelete}>Sí, eliminar</button>
+				<button onClick={onReset}>No, volver</button>
 			</>
 		);
 	} else {
 		return (
 			<>
 				<p>Eliminado con éxito</p>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							confirmed: false,
-							deleted: false,
-							inputValue: "",
-						});
-					}}
-				>
-					Me arrepentí
-				</button>
+				<button onClick={onReset}>Me arrepentí</button>
 			</>
 		);
 	}
